13246eca33437462de96eb10d8924ed427c55e3b
carloscubas <carlos.cubas@gmail.com>
2019-05-28 19:20:46 -0300

    ajuste tutorial

--- src/main/java/br/cubas/usercontrol/UsercontrolApplication.java
+++ src/main/java/br/cubas/usercontrol/UsercontrolApplication.java
@@ -2,9 +2,8 @@ package br.cubas.usercontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude= {SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class UsercontrolApplication {
 
 	public static void main(String[] args) {
